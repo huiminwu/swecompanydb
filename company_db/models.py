900c7b4ece5adc710bbbda8d8ca73bd2f6163b1d
Ananya Jain <ananyaj74@gmail.com>
2020-11-04 17:25:22 -0800

    Created basic Company/CompanyRep models

--- company_db/models.py
+++ company_db/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+SPONSOR_TYPES = [
+    ['NONE', 'Not a Sponsor'],
+    ['BRONZE', 'Bronze'],
+    ['SILVER', 'Silver'],
+    ['GOLD', 'Gold']
+]
+
+REP_ROLES = [
+    ['RECRUITER', 'Recruiter'],
+    ['ENGINEER', 'Engineer'],
+    ['OTHER', 'Other']
+]
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+    sponsorship = models.CharField(choices=SPONSOR_TYPES, default='NONE', max_length=100) # level of sponsorship
+
+    def __str__(self):
+        return self.name
+    
+class CompanyRep(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    active = models.BooleanField(default=True) # status (active or not)
+
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, null=True)
+    role = models.CharField(choices=REP_ROLES, max_length=100)
+
+    notes = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.name + ", " + str(self.company)
