5d1cbaf5f0f3b31154b9902f084ee5413c6b2093
huiminwu <huiminwu24@gmail.com>
2020-11-08 17:01:22 -0500

    Add pages up to tutorial 3

--- company_db/views.py
+++ company_db/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+posts = [
+	{
+		'author': 'CoreyMS', 
+		'title': 'BlogPost',
+		'content': 'first post cont',
+		'date_posted': 'August 27, 2018'
+	},
+	{
+		'author': 'Jane doe', 
+		'title': 'BlogPost 2',
+		'content': 'second post cont',
+		'date_posted': 'August 28, 2018'
+	},
+]
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render(request, 'company_db/home.html', context)
+
+def about(request):
+	return render(request, 'company_db/about.html', {'title': 'About'})
