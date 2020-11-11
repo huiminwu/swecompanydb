df7d53cd7eaf6e6d256b607afd51bbaf47db7296
huiminwu <huiminwu24@gmail.com>
2020-11-11 18:17:50 -0500

    Added some search + modifying
    - Search by company rep name + company
    - Gell all active contacts by company or for everyone

--- company_db/admin.py
+++ company_db/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Company, CompanyRep
 
-admin.site.register(Company)
-admin.site.register(CompanyRep)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    search_fields = ['name', ]
+
+@admin.register(CompanyRep)
+class CompanyRepAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'role')
+    list_filter = ('active', 'company')
+    search_fields = ['name', ]
