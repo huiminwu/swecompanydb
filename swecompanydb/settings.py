2f80163d33098dd98111b9e49900ee885ea28c36
huiminwu <huiminwu24@gmail.com>
2020-12-18 20:17:05 -0500

    Fix typo with heroku in settings

--- swecompanydb/settings.py
+++ swecompanydb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
