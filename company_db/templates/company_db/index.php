6d9e256ca6f1d52df149cdae7648b2070138c923
Ananya Jain <ananyaj74@gmail.com>
2021-01-12 16:10:06 -0800

    Troubleshooting

--- company_db/templates/company_db/index.php
+++
@@ -1,4 +0,0 @@
-<?php include_once("base.html"); ?>
-<?php include_once("home.html"); ?>
-<?php include_once("about.html"); ?>
-<?php include_once("exported.html"); ?>
--- swecompanydb/urls.py
+++ swecompanydb/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('company_db.urls'))
+    path('', admin.site.urls)
 ]
