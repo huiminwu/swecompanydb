69443357e5566a43660659c54d7f48d65000507d
Ananya Jain <ananyaj74@gmail.com>
2021-01-12 16:05:39 -0800

    Troubleshooting

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php include_once("company_db/templates/company_db/base.html"); ?>
-<?php include_once("company_db/templates/company_db/home.html"); ?>
-<?php include_once("company_db/templates/company_db/about.html"); ?>
-<?php include_once("company_db/templates/company_db/exported.html"); ?>
