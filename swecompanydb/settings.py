92602342cda0bf4ccb0140aa8d009815555a6a8c
Ananya Jain <ananyaj74@gmail.com>
2021-01-12 20:22:56 -0800

    Update static root

--- swecompanydb/settings.py
+++ swecompanydb/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/app/staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
